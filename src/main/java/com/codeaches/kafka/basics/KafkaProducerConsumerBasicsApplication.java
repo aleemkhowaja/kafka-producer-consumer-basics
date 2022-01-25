447515675a0c8307b115189f5dfa60f7a062fd47
alim.khowaja@clarivate.com <Khanbaba12$>
2022-01-25 20:23:09 +0500

    Kafka producer and consumer with docker

---
+++ src/main/java/com/codeaches/kafka/basics/KafkaProducerConsumerBasicsApplication.java
@@ -0,0 +1,12 @@
+package com.codeaches.kafka.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerConsumerBasicsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaProducerConsumerBasicsApplication.class, args);
+  }
+}
